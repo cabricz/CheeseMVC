e04a149722403a3bb4eca71e0a8f5810bc35f1d7
Zak Cabric <cabricz@gmail.com>
2018-02-20 00:44:17 -0500

    finish class 4 exercises

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -4,18 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace CheeseMVC.Controllers
 {
     public class CheeseController : Controller
     {
-        static private List<string> Cheeses = new List<string>();
+        static private Dictionary<string,string> cheeses = new Dictionary<string, string>();
 
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.cheeses = Cheeses;
+            ViewBag.cheeses = cheeses;
 
             return View();
         }
@@ -27,9 +25,9 @@ namespace CheeseMVC.Controllers
 
         [HttpPost]
         [Route("/Cheese/Add")]
-        public IActionResult NewCheese(string name)
+        public IActionResult NewCheese(string name, string description)
         {
-            Cheeses.Add(name);
+            cheeses.Add(name, description);
 
             return Redirect("/Cheese");
         }
